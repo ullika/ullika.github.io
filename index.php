903c606019ff02f6577753603d8c1409221f912a
ullika <ullika@code-alliance.de>
2018-04-27 17:29:53 +0200

    set index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ullika
+ * Date: 4/27/18
+ * Time: 5:13 PM
+ */
+
+?> <h3>hello and welcome!</h3> <br>
+have a look at my <a href="doc_root/labtime.php">labtime tool</a>!
